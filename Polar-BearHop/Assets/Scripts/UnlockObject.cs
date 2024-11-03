3cb6292c62a0fc3b2c1a5bc6826ce125ec2bec7c
Ulysses Romero <uly@Ulyssess-MacBook-Pro.local>
2024-11-02 20:06:07 -0500

    Updated Collectible Prefab and Added Main Menu Notification

--- Polar-BearHop/Assets/Scripts/UnlockObject.cs
+++ Polar-BearHop/Assets/Scripts/UnlockObject.cs
@@ -5,13 +5,30 @@ using UnityEngine;
 public class UnlockObject : MonoBehaviour
 {
     // Number that corresponds to fact slot
-    public int objectNumber; 
-    void OnTriggerEnter2D(Collider2D other)
+    public int objectNumber;
+
+    public static int collectedInPlaythrough = 0; 
+
+    void Start()
+    {
+        // Checks if the collectible has been collected
+        if (PlayerPrefs.GetInt("Collectible_" + objectNumber, 0) == 1)
+        {
+            Destroy(gameObject); 
+        }
+    }
+    // [System.Obsolete]
+     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             // Saves the unlocked status
             PlayerPrefs.SetInt("UnlockSlot" + objectNumber, 1); 
+            PlayerPrefs.SetInt("Collectible_" + objectNumber, 1);
+
+            collectedInPlaythrough++; 
+            PlayerPrefs.SetInt("CollectedInPlaythrough", collectedInPlaythrough); 
+
             PlayerPrefs.Save();
             Destroy(gameObject); 
         }
