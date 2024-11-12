bc1762e63e7ff9ae1fae5f9884357e805c32e5cc
Huy Nguyen <162205909+HuyNguyenDePaul@users.noreply.github.com>
2024-11-11 18:59:55 -0600

    Final
    Final project. Mechanics are mostly done.

---
+++ Polar-BearHop/Assets/Scripts/Mechanics/LevelTransition/GoToEndless.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToEndless : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the colliding object is the player capsule
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Reset the current scene
+            SceneManager.LoadScene("InfiniteLevel");
+        }
+    }
+}
---
+++ Polar-BearHop/Assets/Scripts/Mechanics/WaterRises/WaterRiseTwo.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WaterRiseTwo : MonoBehaviour
+{
+    public bool rockEventTriggered;
+    void Update()
+    {
+        // Increase the Y scale by 1 every frame
+        Vector3 currentScale = transform.localScale;
+        currentScale.y += 2 * Time.deltaTime; // Adjust to frame rate
+        if (rockEventTriggered)
+        {
+            currentScale.y += 5; // Add 5 to the Y scale
+            rockEventTriggered = false; // Reset the event trigger
+        }
+
+        transform.localScale = currentScale;
+    }
+
+    // Call this method to trigger the "rock" event
+    public void TriggerRockEvent()
+    {
+        rockEventTriggered = true;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the colliding object is the player capsule
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Reset the current scene
+            SceneManager.LoadScene("LevelTwo");
+        }
+    }
+}
