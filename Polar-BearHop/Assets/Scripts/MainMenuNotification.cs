33c8aa744b075ff88a5767861f7ca86f74d5bacf
DoctorJaws <99218004+DoctorJaws@users.noreply.github.com>
2024-11-11 23:10:56 -0600

    ?

--- Polar-BearHop/Assets/Scripts/MainMenuNotification.cs
+++ Polar-BearHop/Assets/Scripts/MainMenuNotification.cs
@@ -1,36 +1,94 @@
-using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MainMenuNotification : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
-    public Text collectibleNotificationText; 
+
+    public Button[] menuButtons;
+    private int selectedButtonIndex = 0;
 
     void Start()
     {
-        int collectedInLastPlaythrough = PlayerPrefs.GetInt("CollectedInPlaythrough", 0);
+        HighlightButton();
+    }
 
-        if (collectedInLastPlaythrough > 0)
+    void Update()
+    {
+        HandleInput();
+    }
+
+    public void StartGame()
+    {
+        // Resets the number of collected collectibles from last playthrough
+        UnlockObject.collectedInPlaythrough = 0;
+        PlayerPrefs.SetInt("CollectedInPlaythrough", 0);
+
+        SceneManager.LoadScene("LevelOne");
+    }
+
+    public void Unlocks()
+    {
+        // Resets the number of collected collectibles from last playthrough
+        UnlockObject.collectedInPlaythrough = 0;
+        PlayerPrefs.SetInt("CollectedInPlaythrough", 0);
+
+        SceneManager.LoadScene("MenuUnlocks");
+    }
+
+    public void MainScreen()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quitted Game!");
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            ShowNotification(collectedInLastPlaythrough);
+            selectedButtonIndex--;
+            if (selectedButtonIndex < 0)
+            {
+                selectedButtonIndex = menuButtons.Length - 1;
+            }
+            HighlightButton();
         }
-        else
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            collectibleNotificationText.gameObject.SetActive(false); 
+            selectedButtonIndex++;
+            if (selectedButtonIndex >= menuButtons.Length)
+            {
+                selectedButtonIndex = 0;
+            }
+            HighlightButton();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            menuButtons[selectedButtonIndex].onClick.Invoke();
         }
     }
 
-    void ShowNotification(int count)
+    void HighlightButton()
     {
-        collectibleNotificationText.text = "You have collected " + count + " collectibles!";
-        collectibleNotificationText.gameObject.SetActive(true);
-        
-        StartCoroutine(HideNotificationAfterDelay());
-    }
+        foreach (Button button in menuButtons)
+        {
+            var colors = button.colors;
+            colors.normalColor = Color.white;
+            button.colors = colors;
+        }
 
-    IEnumerator HideNotificationAfterDelay()
-    {
-        yield return new WaitForSeconds(5); 
-        collectibleNotificationText.gameObject.SetActive(false);
+        var selectedColors = menuButtons[selectedButtonIndex].colors;
+
+        selectedColors.normalColor = Color.yellow;
+        menuButtons[selectedButtonIndex].colors = selectedColors;
     }
 }
